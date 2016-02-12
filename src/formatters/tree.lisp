45c2b4eea0a492cacff23264d6ea22eae9650892
Ivan Kocienski <ivan.kocienski@gmail.com>
2016-02-12 10:14:44 +0000

    Provide better tree formatter output

--- src/formatters/tree.lisp
+++ src/formatters/tree.lisp
@@ -10,28 +10,49 @@
 		 :initarg delta)))
 
 (defmethod formatter-start-run ((this tree-formatter))
-  (call-next-method)
-  )
+  (call-next-method))
 
 (defmethod formatter-end-run ((this tree-formatter))
-  (call-next-method)
-  )
+  (call-next-method))
 
 (defmethod formatter-start-group ((this tree-formatter) group)
   (call-next-method)
+  (with-slots (indent
+	       indent-string
+	       indent-delta) this
+
+    (format t "~a~a~%" indent-string (spec-group-caption group))
+    (incf indent indent-delta)
+    (setf indent-string (repeat-string indent " ")))
   )
 
 (defmethod formatter-stop-group ((this tree-formatter))
   (call-next-method)
-  )
+  (with-slots (indent
+	       indent-string
+	       indent-delta) this
 
-(defmethod formatter-start-spec ((this tree-formatter) group)
+    (decf indent indent-delta)
+    (setf indent-string (repeat-string indent " "))))
+
+(defmethod formatter-start-spec ((this tree-formatter) spec)
   (call-next-method)
-  )
+  (with-slots (indent-string) this
+    (format t "~a>  ~a~%" indent-string (spec-name spec))))
 
 (defmethod formatter-end-spec ((this tree-formatter) result message)
   (call-next-method)
-  )
+  (with-slots (indent-string) this
+    (format t "~a>    : ~a ~a~%"
+	    indent-string
+	    (cond ((eq result +SPEC-RUN-FAIL+) "FAIL")
+		  ((eq result +SPEC-RUN-PENDING+) "pending")
+		  ((eq result +SPEC-RUN-PASS+) "okay")
+		  (t "maiyunnow?"))
+	    (if (or (eq result +SPEC-RUN-PENDING+)
+		    (eq result +SPEC-RUN-FAIL+))
+		message
+		""))))
 
 (defmethod formatter-report ((this tree-formatter))
   (call-next-method))
